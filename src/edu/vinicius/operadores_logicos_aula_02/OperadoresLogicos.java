36a2ca5a2732e444dc8a9376225af1a44dba3f81
Vinicius Emilio <emiliolyra4000@gmail.com>
2024-05-16 20:52:25 -0300

    Atualizações de Conceitos Básicos

---
+++ src/edu/vinicius/operadores_logicos_aula_02/OperadoresLogicos.java
@@ -0,0 +1,11 @@
+package edu.vinicius.operadores_logicos_aula_02;
+
+public class OperadoresLogicos{
+
+    public static void main(String[] args){
+        
+
+
+    }
+
+}
---
+++ src/edu/vinicius/tipos_e_variaveis_aula_01/TiposVariaveis.java
@@ -0,0 +1,24 @@
+package edu.vinicius.tipos_e_variaveis_aula_01;
+
+public class TiposVariaveis {
+
+    public static void main(String[] args) {
+
+        int idade;
+        String nome = "Vinícius";
+        char letra = 6;
+        System.out.println(letra);
+        System.out.println(nome);
+        idade = 12;
+        System.out.println(idade + " Anos");
+        System.out.println(nome + "Shop");
+        float pi = 3.14F;
+        System.out.println("O valor de Pi pode ser arredondado para: " + pi);
+        boolean vacinacao = false;
+            if (vacinacao == false) {
+                System.out.println("Você precisa se Vacinar!");
+            } 
+        
+    }
+
+}
